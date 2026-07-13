be8a53ee7aa83cb31916ee1c9ec67d1ec0fe45b0
Remco Jansen <remcojansendesign@gmail.com>
2026-07-13 13:45:48 +0200

    vue3 spa - to menu fix - open button was above the menu when opened

--- resources/js/components/account/AccountMenu.vue
+++ resources/js/components/account/AccountMenu.vue
@@ -22,31 +22,35 @@ const handleLogout = async (): Promise<void> => {
 
 <template>
     <div class="relative">
-        <div v-if="showDropdown">
-            <div class="fixed inset-0 z-20 bg-black/45 backdrop-blur-xs" @click="closeDropdown"></div>
 
-            <div class="absolute right-0 z-30 mt-2 w-56 rounded-2xl border border-[#d6d6d1] bg-white shadow-lg ring-1 ring-black/5 dark:border-[#3E3E3A] dark:bg-[#161615]"
-                @click.stop>
+        <div v-if="showDropdown" class="z-40">
+            <div class="fixed inset-0 z-40 bg-black/45 backdrop-blur-xs" @click="closeDropdown"></div>
+
+            <div
+                class="absolute right-0 z-40 mt-2 w-56 rounded-2xl border border-[#d6d6d1] bg-white shadow-lg ring-1 ring-black/5 dark:border-[#3E3E3A] dark:bg-[#161615]">
+
+
                 <div
                     class="border-b border-[#e6e6e2] px-4 py-3 text-sm text-[#6C6C66] dark:border-[#3E3E3A] dark:text-[#A1A19A]">
                     <p class="font-semibold text-[#111113] dark:text-[#EDEDEC]">{{ auth.user.value?.name }}</p>
                     <p class="mt-1 truncate"><strong>Role:</strong> {{ auth.user.value?.role_label }}</p>
                 </div>
 
-                <div class="border-b border-[#e6e6e2] space-y-1 px-3 py-3">
-                    <h4>Admin</h4>
+                <!-- Admin POC -->
+                <div class="border-b border-[#e6e6e2] space-y-1 px-4 py-3 dark:border-[#3E3E3A]">
+                    <h5 class="text-sm font-semibold text-[#111113] dark:text-[#EDEDEC]">Admin</h5>
                     <ul>
                         <li>
                             <!-- convert to Button -->
                             <router-link to="/blog/posts"
-                                class="block rounded-lg px-3 py-2 text-sm text-[#111113] transition-colors hover:bg-[#f53003] hover:text-white dark:text-[#EDEDEC] dark:hover:bg-[#FF4433]">
+                                class="block rounded-lg py-2 pl-3 text-sm text-[#111113] transition-colors hover:bg-[#f53003] hover:text-white dark:text-[#EDEDEC] dark:hover:bg-[#FF4433]">
                                 Dashboard (posts)
                             </router-link>
                         </li>
                         <li>
                             <!-- convert to Button -->
                             <router-link to="/blog/comments"
-                                class="block rounded-lg px-3 py-2 text-sm text-[#111113] transition-colors hover:bg-[#f53003] hover:text-white dark:text-[#EDEDEC] dark:hover:bg-[#FF4433]">
+                                class="block rounded-lg py-2 pl-3 text-sm text-[#111113] transition-colors hover:bg-[#f53003] hover:text-white dark:text-[#EDEDEC] dark:hover:bg-[#FF4433]">
                                 Dashboard (comments)
                             </router-link>
                         </li>
@@ -61,8 +65,10 @@ const handleLogout = async (): Promise<void> => {
             </div>
         </div>
 
-        <Button variant="bordered_normal" size="md" class="relative z-30 px-4 py-2" @click="toggleDropdown" data-test="account-menu-button">
+        <Button variant="bordered_normal" size="md" class="relative z-30 px-4 py-2" @click="toggleDropdown"
+            data-test="account-menu-button">
             {{ auth.user.value?.name ?? 'Account' }}
         </Button>
+
     </div>
 </template>
